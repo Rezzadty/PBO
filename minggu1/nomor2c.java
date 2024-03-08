5dc15ed2189a48c143e4aa6d92dec6c1353fb036
Reza Aditya <112898535+Rezzadty@users.noreply.github.com>
2024-03-08 19:11:05 +0700

    Update nomor2c.java

--- minggu1/nomor2c.java
+++ minggu1/nomor2c.java
@@ -2,9 +2,9 @@ package minggu1; // nama folder
 
 class nomor2c { // nama file project
     public static void main(String[] args) {
-        double tinggitabung = 10;
-        double diametertabung = 5;
-        double volumetabung = Math.PI * Math.pow(diametertabung, 2) * tinggitabung;
+        double tinggitabung = 10; //Double untuk tinggi dari tabung
+        double diametertabung = 5; //Double untuk diamaeter tabung
+        double volumetabung = Math.PI * Math.pow(diametertabung, 2) * tinggitabung; //Double untuk menghitung Volume dari Tabung
 
         System.out.println("Menghitung Volume Tabung\n");
         System.out.println("==============================");
